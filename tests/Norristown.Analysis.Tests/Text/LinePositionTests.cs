c241c68e5c08ad5284188cba7811e131998bd304
Cole Campbell <cole.campbell@twistedlogik.net>
2026-07-31 09:26:02 -0400

    Set up test coverage

---
+++ tests/Norristown.Analysis.Tests/Text/LinePositionTests.cs
@@ -0,0 +1,24 @@
+﻿using Norristown.Analysis.Text;
+
+namespace Norristown.Analysis.Tests.Text
+{
+    public sealed class LinePositionTests
+    {
+        [Fact]
+        public void Constructor_SetsLineAndColumn()
+        {
+            var position = new LinePosition(123, 234);
+            Assert.Equal(123, position.Line);
+            Assert.Equal(234, position.Column);
+        }
+
+        [Theory]
+        [InlineData(0, 0, "(0,0)")]
+        [InlineData(123, 234, "(123,234)")]
+        public void ToString_ReturnsTheCorrectString(Int32 line, Int32 column, String expected)
+        {
+            var position = new LinePosition(line, column);
+            Assert.Equal(expected, position.ToString());
+        }
+    }
+}
